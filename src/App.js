20213968edfc54681c8cdabaf90b92ea5ad3dc07
leseraphina <leseraphina@gamil.com>
2023-05-15 13:23:35 +0900

    basic04 end

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import ProductList from "./components/ProductList";
 import data from './data.json';
 import './App.css';
 
-function App() {
-const [order,setOrder] = useState('date');
-const [items,setItmes] = useState(data);
-// console.log(order);
-// console.log(items);
-const sortItems = items.sort((a,b)=> b[order]-a[order]);
-// function newClick(){}
-const newClick = () => { setOrder('date')}
-const bestClick = () => { setOrder('rating')}
+
+function Option({etc,sortItems}){
+  const etcChange = (e) => {
+    return sortItems(e.target.value);
+    }
   return (
+    <select
+    defaultValue={etc}
+    onChange={etcChange}>
+    <option value="necklaces">necklaces</option>
+    <option value="earrings">earrings</option>
+  </select>
+  )
+}
+function App() {
+const [etc , setEtc] = useState('')
+const [items,setItmes] = useState(data);
+let subData = data;
+// console.log(subData)
+
+const sortItems = (a) => {
+  console.log(a)
+const newItem = subData.filter((item) => {return item.kind === a})
+console.log(newItem)
+setEtc(a);
+setItmes(newItem);
+// setItmes(newItem)
+}
+const DelectItem = (id) =>{
+  const newItem = items.filter((item) => item.id !== id);
+  setItmes(newItem)
+}
+  return ( 
    <div id="wrap">
     <div className="btn">
-      <button type="button" onClick={newClick}>최신순</button>
-      <button type="button" onClick={bestClick}>베스트순</button>
+  
+    <Option etc={etc} sortItems={sortItems} />
     </div>
-    <ProductList items={sortItems} />
+    <ProductList items={items} onDelete={DelectItem} />
    </div>
   );
 }
---
+++ src/App_01.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+import data from './data.json';
+import './App.css';
+
+function App() {
+const [order,setOrder] = useState('date');
+const [items,setItmes] = useState(data);
+// console.log(order);
+// console.log(items);
+const sortItems = items.sort((a,b)=> b[order]-a[order]);
+// function newClick(){}
+const newClick = () => { setOrder('date')}
+const bestClick = () => { setOrder('rating')}
+const DelectItem = (id) =>{
+  const newItem = items.filter((item) => item.id !== id);
+  setItmes(newItem)
+}
+  return (
+   <div id="wrap">
+    <div className="btn">
+      <button type="button" onClick={newClick}>최신순</button>
+      <button type="button" onClick={bestClick}>베스트순</button>
+    </div>
+    <ProductList items={sortItems} onDelete={DelectItem} />
+   </div>
+  );
+}
+
+export default App;
